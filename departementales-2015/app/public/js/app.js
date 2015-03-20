6e729f06539253e99cd2014382c0f577a3598a6d
Julien Marinescu <jma@makina-corpus.com>
2015-03-20 15:06:37 +0100

    legend first test

--- departementales-2015/app/public/js/app.js
+++ departementales-2015/app/public/js/app.js
@@ -5,6 +5,9 @@ var App = function(){
   self.tileLayer;
   self.contourLayer;
   self.map;
+  self.data;
+
+  /** options **/
 
   var options = {
     tileUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
@@ -19,13 +22,17 @@ var App = function(){
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }
 
+  /** **/
+
   var canvas = L.canvas();
 
   var customLayer = L.geoJson(null, {
       onEachFeature: onEachFeature,
       renderer: canvas
   });
+  var regex = new RegExp(/[A-Za-z]+/)
 
+  /** Related to feature **/
 
   function onEachFeature(feature, layer) {
     layer.setStyle({ color: '#f00', weight: 0.4, fillOpacity: 0});
@@ -34,25 +41,64 @@ var App = function(){
       mouseout: resetHighlight
     });
   }
-  function highlightFeature(feature, layer){
-    this.setStyle({'weight': 2, fillOpacity: 0.4});
+
+  function highlightFeature(event){
+    //this.setStyle({'weight': 0.1});
+    var dep = this.feature.properties.DEP;
+    var canton = this.feature.properties.CT;
+    if( !regex.exec(dep) ){
+      dep = parseInt(dep);
+    }
+    if( !regex.exec(canton) ){
+      canton = parseInt(canton);
+    }
+    self.legend.update(self.data[dep].cantons[canton]);
   }
+
   function resetHighlight(feature, layer){
     this.setStyle({'weight': 0.4, fillOpacity: 0})
   }
+  
+  /** Legends **/
 
+  self.legend = L.control({position: 'topright'});
+  
+  self.legend.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'legend info');
+    return this._div;
+  };
+
+
+  self.legend.update = function (data) {
+    var rows = [];
+    $.each(data.binomes, function(){
+      var parti = this.parti;
+      var nom = this.nom;
+      rows.push('<li><div>' + parti + '<br>' + nom + '</div></li>')
+    })
+    this._div.innerHTML = '<b> CANTON : ' + data.libelle + '</b><ul>' + rows.join("") + '</ul>';
+
+  };
+
+  /** Init **/
 
   self.init = function(){
     // init map
     self.map = L.map(options.containerId);
+    self.legend.addTo(self.map)
+
+    $.getJSON("./data/candidatures.json", function(data){
+      self.data = data;
+    });
 
     // center on France
     self.map.setView(new L.LatLng(46.603354,1.8883335), 6);
-    
+      
     // add an OpenStreetMap tile layer
     self.tileLayer =L.tileLayer(options.tileUrl, {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution: options.attribution
     })
+
     if(options.contour.type === "topojson"){
       var contourLayer = omnivore.topojson(options.contour.url, null, customLayer);
       // small fix
