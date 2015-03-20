1e3765b820cf8382fce8b5f15e518e435503c5f4
Julien Marinescu <jma@makina-corpus.com>
2015-03-20 09:46:39 +0100

    init app

---
+++ departementales-2015/app/public/js/app.js
@@ -0,0 +1,67 @@
+var App = function(){
+
+  var self = this;
+  
+  self.tileLayer;
+  self.contourLayer;
+  self.map;
+
+  var options = {
+    tileUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    candidats: {
+
+    },
+    contour: {
+        url: './data/canton_2015-ms.json',
+        type: 'topojson',
+    },
+    containerId: 'map',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }
+
+  var canvas = L.canvas();
+
+  var customLayer = L.geoJson(null, {
+      onEachFeature: onEachFeature,
+      renderer: canvas
+  });
+
+
+  function onEachFeature(feature, layer) {
+    layer.setStyle({ color: '#f00', weight: 1, fillOpacity: 0});
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight
+    });
+  }
+  function highlightFeature(feature, layer){
+    this.setStyle({'weight': 2, fillOpacity: 1});
+  }
+  function resetHighlight(feature, layer){
+    this.setStyle({'weight': 1, fillOpacity: 0})
+  }
+
+
+  self.init = function(){
+    // init map
+   self.map = L.map(options.containerId);
+
+    // center on France
+   self.map.setView(new L.LatLng(46.603354,1.8883335), 6);
+    
+    // add an OpenStreetMap tile layer
+    self.tileLayer =L.tileLayer(options.tileUrl, {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    })
+    if(options.contour.type === "topojson"){
+      var contourLayer = omnivore.topojson(options.contour.url, null, customLayer);
+      // small fix
+      contourLayer.on("dblclick", function(event){
+        self.map.fire("dblclick", event);
+      })
+    }
+
+    contourLayer.addTo(self.map);
+    self.tileLayer.addTo(self.map);
+  }
+}
