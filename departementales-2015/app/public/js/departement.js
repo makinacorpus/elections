eccf98f81ceefa8251d52ea33ebdb9b686602f37
SBats <simon.bats12@gmail.com>
2015-03-23 01:51:07 +0100

    add zommScroll option + example + default values for example + changed vendor-logo to bottomleft place on leaflet map

--- departementales-2015/app/public/js/departement.js
+++ departementales-2015/app/public/js/departement.js
@@ -1,4 +1,4 @@
-var App = function(dataset) {
+var App = function (dataset) {
 
     var departement, pymChild;
 
@@ -24,18 +24,19 @@ var App = function(dataset) {
     var options = {
         tileUrl: 'http://tilestream.makina-corpus.net/v2/osmlight-france/{z}/{x}/{y}.png',
         contour: {
-            url: '../../../resources/carte_elec/carte_elec_dept_'+departement+'.geojson',
+            url: '../../../resources/carte_elec/carte_elec_dept_' + departement + '.geojson',
             type: 'geojson',
         },
+        scrollWheelZoom: dataset.zoomOnScroll || true,
         containerId: 'map',
         attribution: 'Tuiles par <a href="http://makina-corpus.com/expertise/cartographie">Makina Corpus</a> & données &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }
+    };
 
     self.setPym = function (pC) {
         pymChild = pC;
-    }
+    };
 
-    self.init = function(){
+    self.init = function () {
         if (mkcMapFrame && dataset) {
             if (pymChild) {
                 mkcMapFrame.init(dataset, pymChild);
@@ -43,21 +44,22 @@ var App = function(dataset) {
         }
 
         // init map
-        self.map = L.map(options.containerId, {fullscreenControl: true, minZoom: 6, maxZoom: 14, attributionControl: false}).setActiveArea('activeArea');
+        self.map = L.map(options.containerId, {fullscreenControl: true, minZoom: 6, maxZoom: 14, attributionControl: false, scrollWheelZoom: options.scrollWheelZoom}).setActiveArea('activeArea');
         // add an OpenStreetMap tile layer
-        self.tileLayer =L.tileLayer(options.tileUrl, {
+        self.tileLayer = L.tileLayer(options.tileUrl, {
             attribution: options.attribution
         });
         self.tileLayer.addTo(self.map);
 
         // read result from csv
         var results = {};
-        $.getJSON('../../data/resultats/tour1/'+departement+'.json', function(data) {
-            for(var i=1;i<data.length;i++) {
+        $.getJSON('../../data/resultats/tour1/' + departement + '.json', function (data) {
+            var i = 0;
+            for (i = 1; i < data.length; i++) {
                 var bureau = data[i][1];
                 var parti = data[i][3];
                 var score = data[i][4];
-                if(!results[bureau]) {
+                if (!results[bureau]) {
                     results[bureau] = {
                         scores: {},
                         winner: {
@@ -67,7 +69,7 @@ var App = function(dataset) {
                     };
                 }
                 results[bureau].scores[parti] = score;
-                if(parti != 'ABS' && parti != 'NUL' && score > results[bureau].winner.score) {
+                if (parti != 'ABS' && parti != 'NUL' && score > results[bureau].winner.score) {
                     results[bureau].winner = {
                         parti: parti,
                         score: score
@@ -104,16 +106,23 @@ var App = function(dataset) {
             }
 
             var contourLayer = omnivore.geojson(options.contour.url, null, customLayer)
-            .on('ready', function() {
+            .on('ready', function () {
                 self.map.fitBounds(customLayer.getBounds());
             });
             // small fix
-            contourLayer.on("dblclick", function(event){
+            contourLayer.on("dblclick", function (event){
                 self.map.fire("dblclick", event);
             });
 
             contourLayer.addTo(self.map);
         });
+
+        //optionnal logo
+        if (dataset && dataset.logo) {
+            var vendorLogo = L.control({position: 'bottomleft'});
+            vendorLogo.innerHTML = '<img id="vendor-logo" src="' + dataset.logo + '" />';
+            vendorLogo.addTo(self.map);
+        }
 
         // legend
         var legend = L.control({position: 'topright'});
@@ -123,21 +132,21 @@ var App = function(dataset) {
             return this._div;
         };
         legend.update = function (bureau) {
-            var html = '<h3>Résultats à Toulouse</h3>';
+            var html = '<h3>Résultats à Toulouse</h3>',
+                parti;
             if(bureau && results[bureau]) {
-                html+='<ul>';
-                for(var parti in results[bureau].scores) {
-                    html += '<li>'+parti+' '+ results[bureau].scores[parti]+'</li>';
+                html += '<ul>';
+                for(parti in results[bureau].scores) {
+                    html += '<li>' + parti + ' ' + results[bureau].scores[parti] + '</li>';
                 }
-                html+='</ul>';
+                html += '</ul>';
             }
 
             html += 'Survolez un bureau de vote pour plus de détails';
             html += '<a href="http://www.makina-corpus.com" target="_blank"><img id="logo" src="http://makina-corpus.com/++theme++plonetheme.makinacorpuscom/images/logo.png"></a>';
-            (dataset && dataset.logo) && (html += '<img id="vendor-logo" src="' + dataset.logo + '" />');
             this._div.innerHTML = html;
         };
         legend.addTo(self.map);
         L.control.attribution({position: 'topright'}).addTo(self.map);
-    }
-}
+    };
+};
