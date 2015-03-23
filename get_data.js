3c0da059292fd9e8077fcc6c4b0a928697f0384f
Eric BREHAULT <ebrehault@gmail.com>
2015-03-23 11:31:12 +0100

    export spreadsheet to proper json format

---
+++ get_data.js
@@ -0,0 +1,42 @@
+(function () {
+    var gdoc2json = require('./spreadsheet2json/spreadsheet2json.js');
+
+    function groupByCanton(data) {
+        var cantonMap = {};
+        data.forEach(function(item) {
+            handleNonExpressed(item);
+            var canton = item[0],
+                codeCandidat = item[1],
+                nomCandidat = item[3],
+                parti = item[4],
+                votes = item[5];
+            if (!(canton in cantonMap)) {
+                cantonMap[canton] = [];
+            }
+            cantonMap[canton].push({
+                nomCandidat: nomCandidat,
+                parti: parti,
+                votes: parseInt(votes, 10)
+            });
+        });
+        return cantonMap;
+    }
+
+    function convertData(data) {
+        var result = [["Code du canton", "Code bureau", "Binome", "Code Parti", "Voix" ]],
+            data = data.slice(1);
+        data.forEach(function(item) {
+            result.push([item[0], item[2], item[4], item[5], item[6]]);
+        });
+        console.log(result);
+    };
+
+    var spreadsheets_uuids = [
+	'1SNG7NRLkBZoGMGcvd1LQxpdgyYvZJQtB1qVMzOaIoxc',
+    ];
+
+    spreadsheets_uuids.forEach(function(uuid){
+        var data = gdoc2json.get(uuid, convertData);
+    });
+
+}());
