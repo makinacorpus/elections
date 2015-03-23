8d815482c7857af7a1c37b46bda235286a75f020
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-03-23 14:31:40 +0100

    Inject 'Toulouse 1er tour' results

--- json-comb.js
+++ json-comb.js
@@ -8,7 +8,7 @@
     }
 
     function _convertData(data) {
-        var result = [];
+        var result = {};
 
         data.forEach(function(row) {
             var cols = {
@@ -20,23 +20,45 @@
                 parti:        row[5],
                 votes:        row[6]
             };
-            result.push([
+            var id = [cols.canton, cols.bureau, cols.codeCandidat].join('-');
+            result[id] = [
                 cols.canton,
                 cols.bureau,
                 cols.nomCandidat,
                 cols.parti,
                 cols.votes,
                 cols.commune
-            ]);
+            ];
         });
         var d = new Date();
         var filename = d.toISOString().split('T')[0] + '-' + d.getTime() + '.json';
-        fs.writeFile(filename, JSON.stringify(result), _writeCallback);
+
+        fs.readFile('resources/31-elections-departementales-2015-1er-tour.json', { encoding: 'utf8' }, function (err, json) {
+            var data   = JSON.parse(json);
+            var output = [];
+
+            data.forEach(function (r) {
+                for (var i = 1; i <= r.fields.nombre_de_listes_pour_le_canton; i++) {
+                    var resultLine = [r.fields.code_canton, r.fields.numero_du_bureau_de_vote_assn, i].join('-');
+                    var fieldName  = 'nbr_voixliste' + ('abcdefghijkl'[i-1]);
+                    if (result[resultLine]) {
+                        result[resultLine][4] = r.fields[fieldName];
+                    }
+                }
+            });
+            var j;
+            for (j in result) {
+                output.push(result[j]);
+            }
+            console.log(output);
+            // fs.writeFile(filename, JSON.stringify(result), _writeCallback);
+        });
+
     };
 
     var spreadsheets_uuids = [
-        '1SNG7NRLkBZoGMGcvd1LQxpdgyYvZJQtB1qVMzOaIoxc',
-        // '1-txT_1hVHYdOAnEQpnH_w4PlhCVzyr3pEgoV_IYldFI',
+        '1SNG7NRLkBZoGMGcvd1LQxpdgyYvZJQtB1qVMzOaIoxc', // real spreadsheet for dpt 31
+        // '1-txT_1hVHYdOAnEQpnH_w4PlhCVzyr3pEgoV_IYldFI', // test spreadsheet
     ];
 
     spreadsheets_uuids.forEach(function(uuid){
