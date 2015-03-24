66e1afdba7dd208b503a21bbc6a82bbfdd5930f2
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-03-25 00:36:54 +0100

    Add some code comments

--- departementales-2015/app/public/js/mkc-mapframe.js
+++ departementales-2015/app/public/js/mkc-mapframe.js
@@ -4,8 +4,13 @@
 
 	var myModule = function () {
 
+		/**
+		 * Adjust height of a pseudo element
+		 * @param  {number} h The height to set
+		 * @return {null}
+		 */
 		function _changeHeight (h) {
-			var height = parseInt(h, 10);
+			var height    = parseInt(h, 10);
 			var lastStyle = document.styleSheets[document.styleSheets.length -1];
 			if (isFinite(height)) {
 				lastStyle.addRule('#main:before', 'padding-top: ' + height + '%');
@@ -13,13 +18,20 @@
 		}
 
 		function _init (dataset, pymChild) {
+			// If no dataset, don't do anything
 			if (typeof dataset === 'undefined') return;
 
+			// If there is a height in dataset : adjust height of frame
 			dataset.height && _changeHeight(dataset.height);
 
+			// Tell parent frame to adjust to the new frame height
 			pymChild.sendHeight();
 		}
 
+		/**
+		 * Convert current query string to object
+		 * @return {object} Object representation of the query string
+		 */
 		function _getQueryString () {
 			var qs = {}, qsa = [];
 			if (location.search.length) {
@@ -40,6 +52,11 @@
 			return _getQueryString().parti || false;
 		}
 
+		/**
+		 * Generate query string from object
+		 * @param  {object} obj Object to be converted to query string
+		 * @return {string}     Query string
+		 */
 		function _buildQueryString(obj) {
 			var items = [];
 			for(var key in obj) {
