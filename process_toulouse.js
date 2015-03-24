ab5ac3c88dc3e887b352b3586f5a961f216e737a
Simon Georges <simon.georges@makina-corpus.com>
2015-03-24 10:39:39 +0100

    CHG: Fix Toulouse Data

--- process_toulouse.js
+++ process_toulouse.js
@@ -1,7 +1,62 @@
 var jf = require('jsonfile');
 var util = require('util');
 var gdoc2json = require('./spreadsheet2json/spreadsheet2json.js');
-
+var mapping = {
+  "42":2,
+  "93":3,
+  "20":1,
+  "37":3,
+  "117":4,
+  "38":1,
+  "23":1,
+  "3":4,
+  "135":3,
+  "52":4,
+  "77":1,
+  "137":2,
+  "99":2,
+  "133":5,
+  "132":3,
+  "85":5,
+  "45":1,
+  "33":4,
+  "98":5,
+  "62":4,
+  "107":5,
+  "75":4,
+  "125":3,
+  "122":4,
+  "18":4,
+  "43":1,
+  "112":2,
+  "13":4,
+  "121":2,
+  "136":1,
+  "32":2,
+  "68":5,
+  "92":1,
+  "87":3,
+  "17":4,
+  "36":6,
+  "130":2,
+  "63":5,
+  "27":2,
+  "40":3,
+  "89":3,
+  "39":1,
+  "15":3,
+  "51":4,
+  "34":1,
+  "124":5,
+  "6":6,
+  "76":3,
+  "48":3,
+  "88":1,
+  "79":5,
+  "11":2,
+  "58":2,
+  "80":2
+};
 var bureaux = {};
 var listes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
 var file = './departementales-2015/data/resultats/tour1/elections-departementales-2015-1er-tour.json'
@@ -26,8 +81,18 @@ jf.readFile(file, function(err, obj) {
                 } else if(item[4]=='ABSTENTION') {
                     voix = bureaux[bureau]['nombre_d_inscrits'] - bureaux[bureau]['nombre_de_votants_d_apres_les_feuilles_d_emargement'];
                 } else {
-                    var id_liste = item[3];
-                    voix = bureaux[bureau]['nbr_voixliste'+listes[id_liste]];
+                    var id_liste = item[3]; //Spreadsheet 1,2,3,4,5
+                    var i = 0;
+                    while(i < 12) {
+                      var key = "id_liste" + listes[i];
+                      var list_id = bureaux[bureau][key];
+                      var list_num = mapping[list_id];
+                      if (list_num == id_liste) {
+                        break;
+                      }
+                      i++;
+                    }
+                    voix = bureaux[bureau]['nbr_voixliste'+listes[i]];
                 }
                 result.push([item[0], bureau, item[4], item[5], voix]);
             } else {
